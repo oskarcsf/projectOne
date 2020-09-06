0d756cfe32607f6a8f4056506dd20c945bc68fc8
oskarcsf <oskar.s.chen@gmail.com>
2020-09-06 21:58:09 +1000

    feature/ added progress bar animation, changes on swapping cards required

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,31 @@
-$(`.card`).hide();
-$(`.card--home`).show();
+// adding extra class name on the same class "bar" for progress bar animation
+let bar = document.querySelectorAll('.bar');
+for (let i = 0; i < bar.length; i++) {
+    bar[i].className += ' bar' + (i + 1);
+}
 
+// adding progress bar animation
+const progressBarAnimation = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        let width = 1;
+        const addingWidth = setInterval(frame, 10);
+
+        function frame() {
+            if (width >= arr[i]) {
+                clearInterval(addingWidth);
+                i = 0;
+            } else {
+                width++;
+                bar[i].style.width = width + "%";
+            }
+        }
+    }
+}
+
+
+//hidding all cards by default, only show when locationHashChange
 function locationHashChanged() {
+    
     if (location.hash === '#home') {
         $(`.card`).hide();
         $(`.card--home`).show();
@@ -10,8 +34,12 @@ function locationHashChanged() {
     if (location.hash === '#resume') {
         $(`.card`).hide();
         $(`.card--resume`).show();
+        progressBarAnimation([10, 20, 40, 60, 100, 80]);
     }
 
 }
 
+
 window.onhashchange = locationHashChanged;
+$(`.card`).hide();
+$(`.card--home`).show();
